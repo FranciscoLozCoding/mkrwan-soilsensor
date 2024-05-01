87a833cb9d0df371617d904a834acff3c9309e6e
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-05-01 12:41:46 -0500

    edited main to be deployable

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,3 @@
-/*
-  Lora Send And Receive
-  This sketch demonstrates how to send and receive data with the MKR WAN 1300/1310 LoRa module.
-  This example code is in the public domain.
-*/
-
 #include <MKRWAN.h>
 #include "arduino_secrets.h"
 using namespace std;
@@ -13,7 +7,6 @@ LoRaModem modem;
 // Uncomment if using the Murata chip as a module
 // LoRaModem modem(Serial1);
 
-// Please enter your sensitive data in the Secret tab or arduino_secrets.h
 String appEui = SECRET_APP_EUI;
 String appKey = SECRET_APP_KEY;
 
@@ -25,26 +18,10 @@ int UplinkTime = 60000;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  // put your setup code here, to run once:
-  Serial.begin(115200); // Initialize Serial for debuging purposes.
-  while (!Serial);
-  // change this to your regional band (eg. US915, AS923, ...)
-  if (!modem.begin(US915)) {
-    Serial.println("Failed to start module");
-    while (1) {}
-  };
-  Serial.print("Your module version is: ");
-  Serial.println(modem.version());
-  Serial.print("Your device EUI is: ");
-  Serial.println(modem.deviceEUI());
+  modem.begin(US915)
 
   delay(5000); //adding a delay(5000) between modem.begin(US915) and modem.joinOTAA(..) makes joining much more reliable. I am not sure why.
-
   int connected = modem.joinOTAA(appEui, appKey,300000); //five minute timeout
-  if (!connected) {
-    Serial.println("Something went wrong; are you indoor? Move near a window and retry");
-    while (1) {}
-  }
 
   // Set poll interval to 60 secs.
   modem.minPollInterval(60);
@@ -79,37 +56,16 @@ void loop() {
   payload[idx++] = highByte(MoistToSend); //whole number
   payload[idx++] = lowByte(MoistToSend); //decimal number
 
-  //print values being sent
-  Serial.println();
-  Serial.print("Sending values:");
-  Serial.println();
-  Serial.print("Temp: ");
-  Serial.print(t_volt*1000);
-  Serial.println();
-  Serial.print("Moisture: ");
-  Serial.println();
-  Serial.print(m_volt*1000);
-  Serial.println();
-
   //send packets
-  int err;
   modem.beginPacket();
   for (uint8_t c = 0; c < idx; c++) {
     modem.write(payload[c]);
   }
-  err = modem.endPacket(true);
-  if (err > 0) {
-    Serial.println("Message sent correctly!");
-  } else {
-    Serial.println("Error sending message :(");
-    Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
-    Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
-  }
+  modem.endPacket(true);
 
   //check for downlink messages from gateway
   delay(5000);
   if (!modem.available()) {
-    Serial.println("No downlink message received at this time.");
     delay(UplinkTime); //wait until next reading
     return; //no downlink, end iteration
   }
@@ -124,13 +80,7 @@ void loop() {
   int HexValue;
 
   //decript downlink message (only 00,01...05)
-  Serial.print("Received: ");
   for (unsigned int j = 0; j < i; j++) {
-
-    Serial.print(rcv[j] >> 4, HEX);
-    Serial.print(rcv[j] & 0xF, HEX);
-    Serial.print(" ");
-
     //store Hex value
     HexValue = rcv[j] & 0xF;
   }
@@ -169,10 +119,7 @@ void loop() {
     UplinkTime = 60000;
   }
 
-  Serial.println();
-
   delay(UplinkTime); //wait until next reading
-
   return; //end iteration
 }
 
